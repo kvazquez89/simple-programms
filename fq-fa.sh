d990426f9f041803a68776474359490fac62ee82
kvazquez89 <36647397+kvazquez89@users.noreply.github.com>
2018-03-08 16:03:10 -0700

    Update fq-fa.sh

--- fq-fa.sh
+++ fq-fa.sh
@@ -1,18 +1,18 @@
 #!/bin/bash/
 #Taking note, haven't learn how to create a *.sh programm.
 
-echo "Please enter the path to the FASTQ file:"
-read FILE
+#echo "Please enter the path to the FASTQ file:"
+#read FILE
 
-echo "Name of the FASTA file:"
-read NAME
+#echo "Name of the FASTA file:"
+#read NAME
 
-echo "Which are the 3 to 6 first characters in the header of your sequences in the FASTQ file?:"
-read HEADER
+#echo "Which are the 3 to 6 first characters in the header of your sequences in the FASTQ file?:"
+#read HEADER
 
-#FILE=$1 #input -> fastq file
-#NAME=$2 #name the output fasta file
-#HEADER=$3
+FILE=$1 #input -> fastq file
+NAME=$2 #name the output fasta file
+HEADER=$3
 
 grep -A 1 "^$HEADER" $FILE | grep -v "\-\-" | sed -E "s/($HEADER)/>\1/" | sed "s/ /_/" > $NAME
 
